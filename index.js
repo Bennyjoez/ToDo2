7f7637374b9f95832bde7eeca600e55a811d4181
Benson Njuguna <njugunab655@gmail.com>
2022-08-25 08:12:59 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,115 @@
+let list = document.getElementById("list")
+let search = document.getElementById('searchParameter')
+let addTaskBtn = document.getElementById('post');
+let editBtn =document.getElementsByClassName('edit');
+
+
+list.addEventListener('click', function (e) {
+    // delete 
+        // after delete the numbering of the items needs to be updated
+    if(e.target.textContent == "Delete") {
+        e.target.parentElement.parentElement.remove()
+        assignNumbers();
+    }
+
+    //
+    else if (e.target.textContent == "Edit") {
+        edit();
+    };
+
+    // edit function 
+    function edit () {
+        let victim = e.target.parentElement.parentElement
+
+        let titleToEdit = victim.getElementsByClassName('name')[0].innerHTML;
+        let description = victim.getElementsByClassName('description')[0].innerHTML;
+    
+        let editBox = `<h2 class="number"></h2>
+                        <input type="text" id="title" placeholder="Title of task">
+                        <textarea id="description" cols="30" rows="2"   placeholder="Description of the task"></textarea>
+                        <button id="change">Confirm</button>`
+        let editTab = document.createElement('div')
+        editTab.innerHTML = editBox
+        editTab.setAttribute('id', 'addTask')
+        editTab.style.marginLeft = "20%"
+
+        victim.style.display = "none"
+
+        editTab.children[1].value = titleToEdit;
+        editTab.children[2].value = description;
+        // victim.appendChild(editTab)
+        list.replaceChild(editTab, victim)
+        console.log(editTab.children);
+
+        // posting changes
+        let confirmBtn  = document.getElementById('change')
+        confirmBtn.addEventListener('click', post);
+
+        function post () {
+            list.replaceChild(victim, editTab)
+            victim.getElementsByClassName('name')[0].innerHTML = editTab.children[1].value;
+            victim.getElementsByClassName('description')[0].innerHTML = editTab.children[2].value
+            victim.style.display = "flex"
+        }
+
+    }
+
+});
+
+
+
+// numbering the list items
+function assignNumbers() {
+    let numberHolders = Array.from(document.getElementsByClassName('number'));
+
+    let tasks = Array.from(document.getElementsByClassName('content'))
+
+    for(let each of numberHolders) {
+        let index = tasks.indexOf(each.parentElement);
+        let numberAssigned = index += 1
+
+        // assigning the numbers to the placeholders
+        each.innerHTML = numberAssigned
+
+    };
+};
+
+list.addEventListener('change', assignNumbers())
+
+
+addTaskBtn.addEventListener('click', function (e) {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+
+    if(title !== "" && description !== "") {
+        let content = `     <h2 class="number"></h2>
+        <div class="details">
+            <h2 class="name">${title}</h2>
+            <p class="description">${description}</p>
+        </div>
+        <div class="buttons">
+            <button class="delete">Delete</button>
+            <button class="edit">Edit</button>
+        </div>`
+        let newContent = document.createElement('div');
+        newContent.classList.add('content');
+        newContent.innerHTML = content;
+        list.appendChild(newContent);
+
+        document.getElementById('description').value = "";
+        document.getElementById('title').value = ""
+
+
+        assignNumbers();
+    }
+    else if (title !== "" && description == "") {
+        alert(`Input a Description`)
+    }
+    else if (title == "" && description !== "") {
+        alert(`Input a Title`)
+    }
+
+})
+
+
+
